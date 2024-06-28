870f2ae64e4cc41c038d2192874a0555206f45aa
shivakumar123441 <shivakumar123441@gmail.com>
2024-06-28 19:20:49 +0530

    corrected bucket name

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -10,7 +10,7 @@ function run() {
    const distFolder = core.getInput('dist-folder', { required: true });
 
    //upload files
-   const s3Uri = `s3://${blucket}`
+   const s3Uri = `s3://${bucket}`
    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
    core.notice('Hello from my custom java script action!');
