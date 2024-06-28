115f92cd71e31e0adbc421c0f0e5f43bc7290956
shivakumar123441 <shivakumar123441@gmail.com>
2024-06-28 19:18:33 +0530

    added aws secrets and s3 config

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,18 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+//const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 
 function run() {
+   //getting inputs 
+   const bucket = core.getInput('bucket', { required: true });
+   const bucketRegion = core.getInput('bucket-region', { required: true });
+   const distFolder = core.getInput('dist-folder', { required: true });
+
+   //upload files
+   const s3Uri = `s3://${blucket}`
+   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
    core.notice('Hello from my custom java script action!');
 
 }
